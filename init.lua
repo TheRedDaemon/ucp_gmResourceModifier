b8ca42f9e4c3880efe295af51f531cae5319fa20
gynt <gynt@users.noreply.github.com>
2024-02-26 19:11:31 +0100

    enforce function(self, ...) style

--- init.lua
+++ init.lua
@@ -63,10 +63,10 @@ exports.enable = function(self, moduleConfig, globalConfig)
   local requireTable = require("gmResourceModifier.dll") -- loads the dll in memory and runs luaopen_gmResourceModifier
   
   -- no wrapping needed?
-  self.LoadGm1Resource = requireTable.lua_LoadGm1Resource
-  self.FreeGm1Resource = requireTable.lua_FreeGm1Resource
-  self.SetGm = requireTable.lua_SetGm
-  self.LoadResourceFromImage = requireTable.lua_LoadResourceFromImage
+  self.LoadGm1Resource = function(self, ...) return requireTable.lua_LoadGm1Resource end
+  self.FreeGm1Resource = function(self, ...) return requireTable.lua_FreeGm1Resource end
+  self.SetGm = function(self, ...) return requireTable.lua_SetGm end
+  self.LoadResourceFromImage = function(self, ...) return requireTable.lua_LoadResourceFromImage end
   
 
   --[[ modify code ]]--
